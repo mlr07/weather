df02f0c26f88c4af723fc7071204000a2e886e4a
matt rhoads <mrhoads1@gmail.com>
2021-09-19 14:28:17 -0500

    added webstest and streamlined tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,30 +1,30 @@
-# method unit tests
 import pytest
-from app import get_weather, crds, key, units 
+from app import get_weather, crds, key, units, app 
+from webtest import TestApp
 
-# for web integration tests
-# import app
-# import webtest
-# test if get_weather returns 200.
-# if test fails config or openweather is broken.
 
 # run with python -m pytest tests/ from project root
 # this will add root to sys.path
 
+
 def test_get_weather():
     """
-    integration test to confirm that weather data is returned form openweather.
-
-    test uses coordinates, api key, and units from config files in app.
-
-    test passes if data is a list and status code in first response is 200.
-
-    if test fails something is wrong with the method or config files. 
+    functional test to check response from openweather api.
     """
 
     data = get_weather(crds, key, units)
-    assert data
-    assert type(data) == list
-    assert type(data[0]) == dict
-    assert data[0]["cod"] == 200
+
+    for i in data:
+        assert i["cod"] == 200
+
+
+def test_route_weather():
+    """
+    functional test to check weather route.
+    """
+
+    test_app = TestApp(app)
+    resp = test_app.get("/")
+
+    assert resp.status == "200 OK"
 
